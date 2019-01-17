999cfce309929baf9cb29da9e8c2d914cc3cf396
savayer <sava0094@gmail.com>
2019-01-17 14:47:11 +0200

    added menu, customized select, change texts. first page is ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,17 @@
 let form1 = document.getElementById('form1');
 let loader = document.querySelector('.loader');
+let select = document.getElementById('select');
+let selectPlaceholder = document.querySelector('.select-placeholder');
 
 form1.addEventListener('click', (e) => {
     e.preventDefault();
     loader.style.display = 'flex';
+})
+
+select.addEventListener('change', function() {
+    if (this.value) {
+        selectPlaceholder.style.display = 'none'
+    } else {
+        selectPlaceholder.style.display = 'block'
+    }
 })
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,8 +2,18 @@
 
 var form1 = document.getElementById('form1');
 var loader = document.querySelector('.loader');
+var select = document.getElementById('select');
+var selectPlaceholder = document.querySelector('.select-placeholder');
 
 form1.addEventListener('click', function (e) {
     e.preventDefault();
     loader.style.display = 'flex';
+});
+
+select.addEventListener('change', function () {
+    if (this.value) {
+        selectPlaceholder.style.display = 'none';
+    } else {
+        selectPlaceholder.style.display = 'block';
+    }
 });
