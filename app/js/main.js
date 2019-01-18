ee208dc7a59ded288cac82450bf06a2a7098cbc9
savayer <sava0094@gmail.com>
2019-01-18 11:38:33 +0200

    added slick, create navigation steps, svg arrows, setActiveStep script

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,49 @@
+/* first page */
 let form1 = document.getElementById('form1');
 let loader = document.querySelector('.loader');
 let select = document.getElementById('select');
 let selectPlaceholder = document.querySelector('.select-placeholder');
 
-form1.addEventListener('click', (e) => {
-    e.preventDefault();
-    loader.style.display = 'flex';
-})
+if (form1) {
+    form1.addEventListener('click', (e) => {
+        e.preventDefault();
+        loader.style.display = 'flex';
+    })
+}
 
-select.addEventListener('change', function() {
-    if (this.value) {
-        selectPlaceholder.style.display = 'none'
-    } else {
-        selectPlaceholder.style.display = 'block'
+if (select) {
+    select.addEventListener('change', function() {
+        if (this.value) {
+            selectPlaceholder.style.display = 'none'
+        } else {
+            selectPlaceholder.style.display = 'block'
+        }
+    })
+}
+/* for steps */
+/* const setPseudoElContent = (selector, value) => {    
+    document.styleSheets[0].addRule(selector, `content: ${value} !important;`);
+} */
+
+const setActiveStep = (step) => {
+    let nav = document.querySelector('.navigation');
+    let selector = '.step' + (step+2) //+ '::after';    
+    for (let i = 1; i <= step; i++) {
+        nav.querySelector('.step'+i).classList.add('active');        
     }
-})
+    
+    //setPseudoElContent( selector, 'none' );
+   // document.querySelector(selector).classList.add('no-after');
+}
+
+if ($('.navigation')) {
+    $('.navigation').slick({
+        slidesToShow: 3,
+        slidesToScrol: 1,
+        dots: false,
+        infinite: false,
+        rtl: true,
+        nextArrow: '<div class="arrowNav nextArrow"><span> הבא </span><svg viewBox="0 0 64 64"><path id="arrow-left-1" d="M46.077 55.738c0.858 0.867 0.858 2.266 0 3.133s-2.243 0.867-3.101 0l-25.056-25.302c-0.858-0.867-0.858-2.269 0-3.133l25.056-25.306c0.858-0.867 2.243-0.867 3.101 0s0.858 2.266 0 3.133l-22.848 23.738 22.848 23.738z"></path></svg></div>',
+        prevArrow: '<div class="arrowNav prevArrow"><span> קודם </span><svg viewBox="0 0 64 64"><path id="arrow-right-1" d="M17.919 55.738c-0.858 0.867-0.858 2.266 0 3.133s2.243 0.867 3.101 0l25.056-25.302c0.858-0.867 0.858-2.269 0-3.133l-25.056-25.306c-0.858-0.867-2.243-0.867-3.101 0s-0.858 2.266 0 3.133l22.848 23.738-22.848 23.738z"></path></svg></div>'
+    })   
+}
