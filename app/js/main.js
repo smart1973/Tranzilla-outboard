7eb840bb6a21eff5961273938fe03909d15c09f0
savayer <sava0094@gmail.com>
2019-01-22 20:03:51 +0200

    created step 8 except a dropdown (?), changed structure of styles, small fixes step 7, small adding script for step 8

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
             dots: false,
             infinite: false,
             rtl: true,
+            speed: 200,
             nextArrow: '<div class="arrowNav nextArrow"><span> הבא </span><svg viewBox="0 0 64 64"><path id="arrow-left-1" d="M46.077 55.738c0.858 0.867 0.858 2.266 0 3.133s-2.243 0.867-3.101 0l-25.056-25.302c-0.858-0.867-0.858-2.269 0-3.133l25.056-25.306c0.858-0.867 2.243-0.867 3.101 0s0.858 2.266 0 3.133l-22.848 23.738 22.848 23.738z"></path></svg></div>',
             prevArrow: '<div class="arrowNav prevArrow"><span> קודם </span><svg viewBox="0 0 64 64"><path id="arrow-right-1" d="M17.919 55.738c-0.858 0.867-0.858 2.266 0 3.133s2.243 0.867 3.101 0l25.056-25.302c0.858-0.867 0.858-2.269 0-3.133l-25.056-25.306c-0.858-0.867-2.243-0.867-3.101 0s-0.858 2.266 0 3.133l22.848 23.738-22.848 23.738z"></path></svg></div>'
         })   
@@ -137,4 +138,36 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    /* step8 */
+    let right1 = document.getElementById('right1');
+    let right2 = document.getElementById('right2');
+    let descrRight1 = document.getElementById('descr_right1');
+    let descrRight2 = document.getElementById('descr_right2');
+    if (right1 && right1.checked) {
+        descrRight1.style.display = 'block'
+    }
+    if (right2 && right2.checked) {
+        descrRight2.style.display = 'block'
+    }
+    if (right1 && right2) {
+        right1.addEventListener('change', function() {
+            if (this.checked) {
+                descrRight1.style.display = 'block';
+                descrRight2.style.display = 'none';
+            } else {
+                descrRight2.style.display = 'block';
+                descrRight1.style.display = 'none';
+            }
+        })
+        right2.addEventListener('change', function() {
+            if (this.checked) {
+                descrRight2.style.display = 'block';
+                descrRight1.style.display = 'none';
+            } else {
+                descrRight1.style.display = 'block';
+                descrRight2.style.display = 'none';
+            }
+        })
+    }
+    
 })
